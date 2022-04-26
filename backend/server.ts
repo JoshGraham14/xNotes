9ca04051891c43b2bdd7dbe8d470d78ba8498c2f
Josh Graham <joshgraham14@gmail.com>
2022-04-26 19:28:37 -0400

    :heavy_plus_sign::bug: Fixed CORS error

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -12,6 +13,12 @@ const port = process.env.PORT || 5000
 connectDB()
 
 const app = express()
+
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+	})
+)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
