1ee2caa16e3170f16fe62877a57488eb6d9bc59d
Josh Graham <joshgraham14@gmail.com>
2022-04-29 22:52:21 -0400

    :sparkles: Implemented autosave functionality

--- frontend/src/components/note/Note.tsx
+++ frontend/src/components/note/Note.tsx
@@ -1,4 +1,6 @@
-import { INote } from '../../util/data/dataFunctions'
+import React, { useState, useEffect, useCallback } from 'react'
+import { debounce } from 'lodash'
+import { INote, saveNote } from '../../util/data/dataFunctions'
 import './note.css'
 
 interface Props {
@@ -7,10 +9,25 @@ interface Props {
 
 export const Note = (props: Props) => {
 	const { note } = props
+	const [text, setText] = useState('')
+
+	const debouncedSave = useCallback(
+		debounce((text, id) => saveNote(text, id), 1500),
+		[]
+	)
+
+	useEffect(() => {
+		setText(note.content)
+	}, [note])
+
+	const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setText(e.target.value)
+		debouncedSave(text, note._id)
+	}
 
 	return (
 		<div className='note'>
-			<p>{note.content}</p>
+			<textarea onChange={handleTextChange} value={text}></textarea>
 		</div>
 	)
 }
