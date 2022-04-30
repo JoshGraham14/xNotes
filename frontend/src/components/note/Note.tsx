26f6092fe80a78a76060a1b774f6c8ec7aec3034
Josh Graham <joshgraham14@gmail.com>
2022-04-30 15:18:00 -0400

    :bug::art: Improved autosave functionality

--- frontend/src/components/note/Note.tsx
+++ frontend/src/components/note/Note.tsx
@@ -1,33 +1,44 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import { debounce } from 'lodash'
-import { INote, saveNote } from '../../util/data/dataFunctions'
+import { getOneNote, INote, saveNote } from '../../util/data/dataFunctions'
 import './note.css'
 
 interface Props {
-	note: INote
+	noteId: string
 }
 
 export const Note = (props: Props) => {
-	const { note } = props
+	const { noteId } = props
 	const [text, setText] = useState('')
+	const [message, setMessage] = useState('')
 
 	const debouncedSave = useCallback(
-		debounce((text, id) => saveNote(text, id), 1500),
+		debounce(async (text, id) => {
+			setMessage('Saving ...')
+			await saveNote(text, id)
+			setMessage('Saved')
+		}, 1000),
 		[]
 	)
 
 	useEffect(() => {
-		setText(note.content)
-	}, [note])
+		const getData = async () => {
+			const data = await getOneNote(noteId)
+			setText(data.content)
+		}
+		getData()
+	}, [noteId])
 
 	const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		setText(e.target.value)
-		debouncedSave(text, note._id)
+		debouncedSave(e.target.value, noteId)
 	}
 
 	return (
 		<div className='note'>
 			<textarea onChange={handleTextChange} value={text}></textarea>
+			<br />
+			<p>{message}</p>
 		</div>
 	)
 }
